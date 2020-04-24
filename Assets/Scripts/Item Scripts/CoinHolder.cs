021bd5cb079a99f1326fcf581576f73c446a599c
Henrique Silva <hpsilva@pm.me>
2020-04-24 02:17:25 -0300

    Fix format of some files.

--- Assets/Scripts/Item Scripts/CoinHolder.cs
+++ Assets/Scripts/Item Scripts/CoinHolder.cs
@@ -1,42 +1,34 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinHolder : MonoBehaviour
 {
-
     private List<CoinScript.CoinType> coinList;
     private void OnTriggerEnter2D(Collider2D other)
     {
         CoinScript coin = other.GetComponent<CoinScript>();
         if (coin != null)
         {
-            PickUpCoin(coin.GetCoinType());
+            PickUpCoin(other.gameObject);
         }
-
-
     }
 
-    private void PickUpCoin(CoinScript.CoinType coinType)
+    private void PickUpCoin(GameObject myCoin)
     {
-
-        switch (coinType)
+        switch (myCoin.GetComponent<CoinScript>().GetCoinType())
         {
-            default:
-            case CoinScript.CoinType.Brown: BrownCoin(); break;
+            case CoinScript.CoinType.Brown: BrownCoin(myCoin); break;
             case CoinScript.CoinType.Purple: PurpleCoin(); break;
         }
-
     }
 
-    private void BrownCoin()
+    private void BrownCoin(GameObject o)
     {
-
+        Destroy(o);
     }
 
     private void PurpleCoin()
     {
-
+        Debug.Log("collided with purple coin");
     }
 }
--- Assets/Scripts/Item Scripts/CoinScript.cs
+++ Assets/Scripts/Item Scripts/CoinScript.cs
@@ -1,26 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
-
-    //[SerializeField] private BoxCollider2D myBoxCollider2D;
-
-    // Start is called before the first frame update
-
-    // private bool isDead = false;
-
-    //[SerializeField] LayerMask m_WhatToCollideWith;
-    // [SerializeField] private int m_CoinScoreValue = 1;
-    // [SerializeField] private int m_PurpleCoinExtraJumps = 1;
-
-    // [SerializeField] GameObject m_CoinPurple;
-
-    [SerializeField] CoinType coinType;
-
-
-
+    [SerializeField] private readonly CoinType coinType;
 
     public enum CoinType
     {
@@ -29,47 +11,8 @@ public class CoinScript : MonoBehaviour
 
     }
 
-
-
-
-
-    // private void OnTriggerEnter2D(Collider2D collision)
-    // {
-
-
-
-    //     if (isDead == false && (collision.gameObject.layer == 8)) // 8 is the player layer
-    //     {
-    //         Destroy(gameObject);
-    //         //Debug.Log("Coin named: " + gameObject.name + " was destroyed by: " + collision.name);
-    //         GameObject.Find("Player").GetComponent<PlayerMovement>().currentScore += m_CoinScoreValue;
-    //         isDead = true;
-    //         PurpleCoin(collision.gameObject);
-
-    //     }
-
-
-
-
-    // }
-
-
-    // private void PurpleCoin(GameObject myUnit)
-    // {
-    //     if (gameObject == m_CoinPurple)
-    //     {
-    //         //Debug.Log(myUnit.GetComponentInParent<CharacterController2D>());
-    //         myUnit.GetComponentInParent<CharacterController2D>().DoubleJumpsRemaining += m_PurpleCoinExtraJumps;
-    //         //myUnit.GetComponent<CharacterController2D>().IsFalling = false;
-
-    //     }
-    // }
-
     public CoinType GetCoinType()
     {
         return coinType;
     }
-
-
-
 }
