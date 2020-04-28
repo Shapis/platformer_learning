85c922b5f3d6c502903203bd6595568658477a40
Henrique Silva <hpsilva@pm.me>
2020-04-27 21:27:47 -0300

    Add Level 03.

---
+++ Assets/Scripts/Item Scripts/ChestGrabber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ChestGrabber : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.GetComponent<ChestScript>() != null)
+        {
+            Debug.Log(other.gameObject);
+        }
+    }
+}
