7b450f534e3d0c4aa7c4af742873ea7c1ed878c1
Shapis <hpsilva@pm.me>
2020-04-28 18:01:08 -0300

    Add LootTable and LootHandler system (#4)

---
+++ Assets/Scripts/LootTable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootTable : MonoBehaviour
+{
+
+    [SerializeField] private ItemType m_ItemType;
+
+
+    public enum ItemType
+    {
+        BrownCoin,
+        PurpleCoin,
+    }
+
+
+    public ItemType GetItemType()
+    {
+        return m_ItemType;
+    }
+}
