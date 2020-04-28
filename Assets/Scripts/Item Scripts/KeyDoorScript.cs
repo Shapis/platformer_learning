823c29b34143d47b949caf42816033e3a1df0258
Henrique Silva <hpsilva@pm.me>
2020-04-28 19:06:15 -0300

    Fix bugs in KeyDoorScript

--- Assets/Scripts/Item Scripts/KeyDoorScript.cs
+++ Assets/Scripts/Item Scripts/KeyDoorScript.cs
@@ -8,31 +8,15 @@ public class KeyDoorScript : MonoBehaviour
 
     [SerializeField] private BoxCollider2D[] m_BoxColliders;
 
-    [SerializeField] private SpriteRenderer m_KeySpriteRenderer;
+    [SerializeField] private GameObject m_GateKeyType;
 
     private bool isOpenPermanently = false;
 
     private void Awake()
     {
-
-        m_KeySpriteRenderer.color = KeyScript.GetKeyColor32(m_KeyType);
-        // keySprite = GameObject.Find("GateKeyType").GetComponent<SpriteRenderer>();
-
-        // switch (keyType)
-        // {
-        //     default:
-        //     case KeyScript.KeyType.Blue: keySprite.sprite = myKeySprites[0]; break;
-        //     case KeyScript.KeyType.Brown: keySprite.sprite = myKeySprites[1]; break;
-        //     case KeyScript.KeyType.Gray: keySprite.sprite = myKeySprites[2]; break;
-        //     case KeyScript.KeyType.Green: keySprite.sprite = myKeySprites[3]; break;
-        //     case KeyScript.KeyType.Purple: keySprite.sprite = myKeySprites[4]; break;
-        //     case KeyScript.KeyType.Silver: keySprite.sprite = myKeySprites[5]; break;
-        //     case KeyScript.KeyType.Teal: keySprite.sprite = myKeySprites[6]; break;
-        //     case KeyScript.KeyType.Violet: keySprite.sprite = myKeySprites[7]; break;
-        //     case KeyScript.KeyType.White: keySprite.sprite = myKeySprites[8]; break;
-
-        // }
-        // //keyImage.sprite = GameObject.Find("KeyRed").GetComponent<Sprite>();
+        m_GateKeyType.GetComponent<SpriteRenderer>().color = KeyScript.GetKeyColor32(m_KeyType);
+        // myKey = gameObject.transform.GetChild(1).GetComponent<SpriteRenderer>();
+        // myKey.color = KeyScript.GetKeyColor32(m_KeyType);
     }
 
 
@@ -45,26 +29,23 @@ public class KeyDoorScript : MonoBehaviour
     {
         if (!isOpenPermanently)
         {
-            LeanTween.cancelAll();
+            //LeanTween.cancelAll();
             m_Animator.SetBool("isOpen", true);
             LeanTween.delayedCall(0.3f, DisableDoorColliders);
-            LeanTween.scale(m_KeySpriteRenderer.gameObject, new Vector3(1.2f, 1.2f, 1.2f), 0.3f);
-            LeanTween.moveLocal(m_KeySpriteRenderer.gameObject, new Vector3(0f, 1.8f, 0f), 0.7f);
+            LeanTween.scale(m_GateKeyType, new Vector3(1.2f, 1.2f, 1.2f), 0.3f);
+            LeanTween.moveLocal(m_GateKeyType, new Vector3(0f, 1.8f, 0f), 0.7f);
             // Destroy(keySprite.gameObject, 0.3f);
         }
-
-
     }
 
     public void CloseDoor()
     {
         if (!isOpenPermanently)
         {
-            LeanTween.cancelAll();
             m_Animator.SetBool("isOpen", false);
             LeanTween.delayedCall(0.3f, EnableDoorColliders);
-            LeanTween.scale(m_KeySpriteRenderer.gameObject, new Vector3(0.6f, 0.6f, 0.6f), 0.3f);
-            LeanTween.moveLocal(m_KeySpriteRenderer.gameObject, new Vector3(0f, -0.214f, 0f), 0.7f);
+            LeanTween.scale(m_GateKeyType, new Vector3(0.6f, 0.6f, 0.6f), 0.3f);
+            LeanTween.moveLocal(m_GateKeyType, new Vector3(0f, -0.214f, 0f), 0.7f);
         }
     }
 
