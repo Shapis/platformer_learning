3d2aa519ef94d99184a94bd4b014b97ee0e45d09
Henrique Silva <hpsilva@pm.me>
2020-04-30 18:37:50 -0300

    Add a centralized Input Handler system.

---
+++ Assets/Scripts/Map Scripts/Node.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [Header("Level Info")]
+    [SerializeField] private string m_LevelName;
+    [SerializeField] private SceneHandler.Scene m_Scene;
+
+    [Header("Node Destinations")]
+    [SerializeField] private GameObject upDestination;
+    [SerializeField] private GameObject downDestination;
+    [SerializeField] private GameObject leftDestination;
+    [SerializeField] private GameObject rightDestination;
+
+
+    [Header("Dependencies")]
+    [SerializeField] private GameObject myPlayer;
+
+    [SerializeField] private InputHandler myInputHandler;
+
+    private bool canMove;
+
+
+
+
+    IEnumerator DoUp()
+    {
+        yield return new WaitForSeconds(1 / 60);
+        while (myPlayer.transform.position != upDestination.transform.position)
+        {
+            myPlayer.transform.position = Vector3.MoveTowards(myPlayer.transform.position, upDestination.transform.position, 8f * Time.deltaTime);
+            yield return null;
+        }
+    }
+    IEnumerator DoDown()
+    {
+        yield return new WaitForSeconds(1 / 60);
+        while (myPlayer.transform.position != downDestination.transform.position)
+        {
+            myPlayer.transform.position = Vector3.MoveTowards(myPlayer.transform.position, downDestination.transform.position, 8f * Time.deltaTime);
+            yield return null;
+        }
+    }
+    IEnumerator DoLeft()
+    {
+        yield return new WaitForSeconds(1 / 60);
+        while (myPlayer.transform.position != leftDestination.transform.position)
+        {
+            myPlayer.transform.position = Vector3.MoveTowards(myPlayer.transform.position, leftDestination.transform.position, 8f * Time.deltaTime);
+            yield return null;
+        }
+    }
+    IEnumerator DoRight()
+    {
+        yield return new WaitForSeconds(1 / 60);
+        while (myPlayer.transform.position != rightDestination.transform.position)
+        {
+            myPlayer.transform.position = Vector3.MoveTowards(myPlayer.transform.position, rightDestination.transform.position, 8f * Time.deltaTime);
+            yield return null;
+        }
+
+    }
+}
--- Assets/Scripts/Resources/InputScripts/InputHandler.cs
+++ Assets/Scripts/Resources/InputScripts/InputHandler.cs
@@ -1,36 +1,237 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿using System;
+using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    [SerializeField] private bool m_DebugLogsEnabled = false;
 
+    // Cancel events //////////////////////////////////////////////////////
+    public event EventHandler OnCancelPressedEvent;
+    public event EventHandler OnCancelUnpressedEvent;
+    ///////////////////////////////////////////////////////////////////////
 
-    [SerializeField] private GameObject m_EscMenu;
+    // Horizontal events //////////////////////////////////////////////////
+    public event EventHandler OnHorizontalLeftPressedEvent;
+    public event EventHandler OnHorizontalLeftUnpressedEvent;
+    public event EventHandler OnHorizontalRightPressedEvent;
+    public event EventHandler OnHorizontalRightUnpressedEvent;
+    private int horizontalDirection;
+    private bool horizontalBusy;
+    ///////////////////////////////////////////////////////////////////////
 
-    public UnityEvent[] OnEscapePress = new UnityEvent[2];
+    // Vertical Events ////////////////////////////////////////////////////
+    public event EventHandler OnVerticalUpPressedEvent;
+    public event EventHandler OnVerticalUpUnpressedEvent;
+    public event EventHandler OnVerticalDownPressedEvent;
+    public event EventHandler OnVerticalDownUnpressedEvent;
+    private int verticalDirection;
+    private bool verticalBusy;
+    ///////////////////////////////////////////////////////////////////////
 
-
-
-
-    private void Start()
-    {
-        OnEscapePress[0]?.AddListener(GameHandler.Pause);
-        OnEscapePress[1]?.AddListener(GameHandler.Resume);
-    }
+    // Jump Events ////////////////////////////////////////////////////////
+    public event EventHandler OnJumpPressedEvent;
+    public event EventHandler OnJumpUnpressedEvent;
+    ///////////////////////////////////////////////////////////////////////
 
     // Update is called once per frame
     void Update()
     {
+        // Cancel events //////////////////////////////////////////////////////
         if (Input.GetButtonDown("Cancel"))
         {
-            if (!m_EscMenu.GetComponent<PopupMenuController>().IsActive)
+            OnCancelPressed();
+            if (m_DebugLogsEnabled)
             {
-                OnEscapePress[0]?.Invoke();
-            }
-            else
-            {
-                OnEscapePress[1].Invoke();
+                Debug.Log("Cancel button pressed!");
             }
         }
+        if (Input.GetButtonUp("Cancel"))
+        {
+            OnCancelUnpressed();
+            if (m_DebugLogsEnabled)
+            {
+                Debug.Log("Cancel button unpressed!");
+            }
+        }
+        ///////////////////////////////////////////////////////////////////////
+
+        // Horizontal events //////////////////////////////////////////////////
+        if (Input.GetButton("Horizontal"))
+        {
+            if (!horizontalBusy)
+            {
+                horizontalBusy = true;
+                if (Input.GetAxisRaw("Horizontal") < 0)
+                {
+                    OnHorizontalLeftPressed();
+                    horizontalDirection = -1;
+                    if (m_DebugLogsEnabled)
+                    {
+                        Debug.Log("Left button pressed!");
+                    }
+                }
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    OnHorizontalRightPressed();
+                    horizontalDirection = 1;
+                    if (m_DebugLogsEnabled)
+                    {
+                        Debug.Log("Right button pressed!");
+                    }
+                }
+            }
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            if ((Input.GetAxisRaw("Horizontal") >= 0 && horizontalDirection < 0))
+            {
+                OnHorizontalLeftUnpressed();
+                horizontalBusy = false;
+                if (m_DebugLogsEnabled)
+                {
+                    Debug.Log("Left button unpressed!");
+                }
+            }
+            else if (Input.GetAxisRaw("Horizontal") <= 0 && horizontalDirection > 0)
+            {
+                OnHorizontalRightUnpressed();
+                horizontalBusy = false;
+                if (m_DebugLogsEnabled)
+                {
+                    Debug.Log("Right button unpressed!");
+                }
+            }
+        }
+        ///////////////////////////////////////////////////////////////////////
+
+        // Vertical Events ////////////////////////////////////////////////////
+        if (Input.GetButton("Vertical"))
+        {
+            if (!verticalBusy)
+            {
+                verticalBusy = true;
+                if (Input.GetAxisRaw("Vertical") < 0)
+                {
+                    OnVerticalDownPressed();
+                    verticalDirection = -1;
+                    if (m_DebugLogsEnabled)
+                    {
+                        Debug.Log("Down button pressed!");
+                    }
+                }
+                if (Input.GetAxisRaw("Vertical") > 0)
+                {
+                    OnVerticalUpPressed();
+                    verticalDirection = 1;
+                    if (m_DebugLogsEnabled)
+                    {
+                        Debug.Log("Up button pressed!");
+                    }
+                }
+            }
+        }
+        if (Input.GetButtonUp("Vertical"))
+        {
+
+            if ((Input.GetAxisRaw("Vertical") >= 0 && verticalDirection < 0))
+            {
+                OnVerticalDownUnpressed();
+                verticalBusy = false;
+                if (m_DebugLogsEnabled)
+                {
+                    Debug.Log("Down button unpressed!");
+                }
+            }
+            else if (Input.GetAxisRaw("Vertical") <= 0 && verticalDirection > 0)
+            {
+                OnVerticalUpUnpressed();
+                verticalBusy = false;
+                if (m_DebugLogsEnabled)
+                {
+                    Debug.Log("Up button unpressed!");
+                }
+            }
+        }
+        ///////////////////////////////////////////////////////////////////////
+
+        // Jump Events ////////////////////////////////////////////////////////
+        if (Input.GetButtonDown("Jump"))
+        {
+            OnJumpPressed();
+            if (m_DebugLogsEnabled)
+            {
+                Debug.Log("Jump button pressed!");
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            OnJumpUnpressed();
+            if (m_DebugLogsEnabled)
+            {
+                Debug.Log("Jump button unpressed!");
+            }
+        }
+        ///////////////////////////////////////////////////////////////////////
     }
+
+    // Cancel events //////////////////////////////////////////////////////
+    private void OnCancelPressed()
+    {
+        OnCancelPressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnCancelUnpressed()
+    {
+        OnCancelUnpressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    ///////////////////////////////////////////////////////////////////////
+
+    // Vertical Events ////////////////////////////////////////////////////
+    private void OnVerticalUpPressed()
+    {
+        OnVerticalUpPressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnVerticalUpUnpressed()
+    {
+        OnVerticalUpUnpressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnVerticalDownPressed()
+    {
+        OnVerticalDownPressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnVerticalDownUnpressed()
+    {
+        OnVerticalDownUnpressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    ///////////////////////////////////////////////////////////////////////
+
+    // Horizontal events //////////////////////////////////////////////////
+    private void OnHorizontalLeftPressed()
+    {
+        OnHorizontalLeftPressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnHorizontalLeftUnpressed()
+    {
+        OnHorizontalLeftUnpressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnHorizontalRightPressed()
+    {
+        OnHorizontalRightPressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnHorizontalRightUnpressed()
+    {
+        OnHorizontalRightUnpressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    ///////////////////////////////////////////////////////////////////////
+
+    // Jump Events ////////////////////////////////////////////////////////
+    private void OnJumpPressed()
+    {
+        OnJumpPressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    private void OnJumpUnpressed()
+    {
+        OnJumpUnpressedEvent?.Invoke(this, EventArgs.Empty);
+    }
+    ///////////////////////////////////////////////////////////////////////
 }
