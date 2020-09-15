2f1fab4d118161e4388e0e47519291a3892ac1a8
Henrique Silva <hpsilva+git@pm.me>
2020-09-15 16:48:21 -0300

    Add overworld water animations

---
+++ Assets/Scripts/Overworld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Overworld : MonoBehaviour
+{
+    [SerializeField] private Sprite[] m_OverworldSprites = new Sprite[4];
+    private Coroutine spriteChangerCoroutine;
+    private int myCurrentColor = 0;
+
+    void Start()
+    {
+        spriteChangerCoroutine = StartCoroutine("ChangeColors");
+    }
+
+    private IEnumerator ChangeColors()
+    {
+        while (true)
+        {
+            if (myCurrentColor >= m_OverworldSprites.Length)
+            {
+                myCurrentColor = 0;
+            }
+            gameObject.GetComponent<SpriteRenderer>().sprite = m_OverworldSprites[myCurrentColor];
+            myCurrentColor++;
+            yield return new WaitForSecondsRealtime(1.5f);
+        }
+    }
+}
