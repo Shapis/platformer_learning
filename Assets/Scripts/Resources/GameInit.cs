d4f594ac0c0380928fe005cbf46a20d810e5f6dd
Henrique Silva <hpsilva+git@pm.me>
2022-05-10 15:45:07 -0300

    Fix screen refresh rate on android builds

---
+++ Assets/Scripts/Resources/GameInit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour
+{
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = Screen.currentResolution.refreshRate;
+    }
+}
